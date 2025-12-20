d0a086575e0774c1d1202048901f39cf882e9b61
ajaylodha246-commits <ajaylodha246@gmail.com>
2025-12-20 15:32:44 +0530

    Create NumberGuessingGame.java

---
+++ Task-1/NumberGuessingGame.java
@@ -0,0 +1,55 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
+        int roundsWon = 0;
+        boolean playAgain = true;
+
+        while (playAgain) {
+            int numberToGuess = random.nextInt(100) + 1;
+            int attempts = 0;
+            int maxAttempts = 5;
+            boolean guessedCorrectly = false;
+
+            System.out.println("\nGuess the number between 1 and 100");
+            System.out.println("You have " + maxAttempts + " attempts.");
+
+            while (attempts < maxAttempts) {
+                System.out.print("Enter your guess: ");
+                int guess = sc.nextInt();
+                attempts++;
+
+                if (guess == numberToGuess) {
+                    System.out.println("Correct! You guessed the number.");
+                    guessedCorrectly = true;
+                    roundsWon++;
+                    break;
+                } else if (guess > numberToGuess) {
+                    System.out.println("Too high!");
+                } else {
+                    System.out.println("Too low!");
+                }
+            }
+
+            if (!guessedCorrectly) {
+                System.out.println("Out of attempts! The number was: " + numberToGuess);
+            }
+
+            System.out.println("Rounds won: " + roundsWon);
+            System.out.print("Do you want to play again? (yes/no): ");
+            String choice = sc.next();
+
+            if (!choice.equalsIgnoreCase("yes")) {
+                playAgain = false;
+            }
+        }
+
+        System.out.println("Thank you for playing!");
+        sc.close();
+    }
+}
